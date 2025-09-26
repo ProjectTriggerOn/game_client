d62ee3876be7569f7c4bd38bddcd4e4bb5c7d996
pisces <pisces-trio@outlook.jp>
2025-09-26 18:45:36 +0900

    Field Shader

---
+++ shader_field.h
@@ -0,0 +1,26 @@
+#pragma once
+/*==============================================================================
+
+   �V�F�[�_�[ [Shader_Field.h]
+														 Author : Youhei Sato
+														 Date   : 2025/05/15
+--------------------------------------------------------------------------------
+
+==============================================================================*/
+#ifndef SHADER_Field_H
+#define	SHADER_Field_H
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+bool Shader_Field_Initialize(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+void Shader_Field_Finalize();
+
+void Shader_Field_SetMatrix(const DirectX::XMMATRIX& matrix);
+void Shader_Field_SetWorldMatrix(const DirectX::XMMATRIX& matrix);
+void Shader_Field_SetViewMatrix(const DirectX::XMMATRIX& matrix);
+void Shader_Field_SetProjectMatrix(const DirectX::XMMATRIX& matrix);
+
+void Shader_Field_Begin();
+
+#endif 
